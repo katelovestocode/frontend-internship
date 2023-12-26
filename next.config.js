ccf6ef580864993a7794fde791aa04d43d2aaa97
vaskevichk <kate.panasiuk@yahoo.com>
2023-12-26 18:50:28 +0200

    edit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
 
-module.exports = {
-  output: "standalone",
-};
+// module.exports = {
+//   output: "standalone",
+// };
